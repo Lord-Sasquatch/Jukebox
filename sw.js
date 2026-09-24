cce9bb6bf4fdb518b210142bbf40e46de74e0a64
Lord-Sasquatch <stephen.fagan69@gmail.com>
2026-09-24 13:42:01 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const C='jukebox-v5';const A=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))});
